21acbdafda908426220b23cf30f0ec4d7d91d29f
Emile Baelde <emilbael@sudent.arteveldehs.be>
2017-11-06 15:06:59 +0100

    index.html toegevoegd

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,14 +5,26 @@ var gulpSass    = require('gulp-sass');
 //stattic server & watching scss /html files
 gulp.task('serve', function() {
     browserSync.init({
-        server: './app'
+        server: './app',
+        port: 8080,
+        ui: {
+            port: 8081,
+            weinre:{
+                port:9081
+            }
+        }
     });
 
     gulp.watch('app/scss/*.scss', ['sass']);
+    gulp.watch('app/*.html').on('change', browserSync.reload) //server harlaad als er iets veranderd
 });
 // Compile sass into CSS
 gulp.task('sass', function(){
     return gulp.src('app/scss/*.scss')
         .pipe(gulpSass())
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
 });
+
+//default task
+gulp.task('default', ['serve'])
