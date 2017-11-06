db75ff2347f9e682568b29800d82106e8c7132e0
Emile Baelde <emilbael@sudent.arteveldehs.be>
2017-11-06 14:23:14 +0100

    gulp file: sass to css and static server

---
+++ Gulpfile.js
@@ -0,0 +1,18 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var gulpSass    = require('gulp-sass');
+
+//stattic server & watching scss /html files
+gulp.task('serve', function() {
+    browserSync.init({
+        server: './app'
+    });
+
+    gulp.watch('app/scss/*.scss', ['sass']);
+});
+// Compile sass into CSS
+gulp.task('sass', function(){
+    return gulp.src('app/scss/*.scss')
+        .pipe(gulpSass())
+        .pipe(gulp.dest('app/css'));
+});
